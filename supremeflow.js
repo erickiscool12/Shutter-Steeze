b295a02938e3a80d990e2b95a5052a3ac951d6b1
erickiscool12 <erick.woodard15@gmail.com>
2017-06-28 17:27:48 -0400

    Work from wednesday

---
+++ supremeflow.js
@@ -0,0 +1,22 @@
+console.log("this works");
+
+var cookies = [
+  "chocolate chip",
+  "the lemon ones",
+  "thin mints",
+  "oreo",
+  "samoa",
+  "oatmeal"
+];
+
+function eat(food) {
+  console.log(' i just ate ' + cookies);
+}
+
+var eatbutton = $(".devour")
+eatbutton.on("click",
+)
+
+for (var 1 = 0; i < cookies.length; i= i + 1) {
+  eat(cookies[i]); }
+}
